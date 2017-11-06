fccc7b59e742bc887580c91c2c2dbeae2c85caee
Seb <seb@takeflight.com.au>
2017-11-06 13:14:40 +1100

    Allow for custom image models, 404 on image not found intead of error

--- wagtailannotatedimage/views.py
+++ wagtailannotatedimage/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
-from wagtail.wagtailimages.models import Filter, Image
+from django.shortcuts import get_object_or_404
+from wagtail.wagtailimages.models import Filter, get_iamge_model
+
+Image = get_iamge_model()
 
 
 def get_full_image_url(request, image_id):
-    image = Image.objects.get(id=image_id)
+    image = get_object_or_404(Image, id=image_id)
     if image:
         filter, _ = Filter.objects.get_or_create(spec='original')
         orig_rendition = image.get_rendition(filter)
