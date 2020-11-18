9cda823cbcf5fd12cd3c1110ab18ef6a0e339e79
Seb <seb@neonjungle.studio>
2020-11-18 13:46:00 +1100

    Start of recplacing travis with gitlab pipelines

--- setup.py
+++ setup.py
@@ -34,18 +34,15 @@ setup(
     include_package_data=True,
     package_data={},
 
-
-
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Framework :: Django',
         'License :: OSI Approved :: BSD License',
     ],
